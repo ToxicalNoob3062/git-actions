6f28893650854f69495645c273fd7a6ed8bef850
ToxicalNoob3062 <rahat3062@gmail.com>
2025-09-10 20:01:23 -0400

    improved the workflow for running integration tests and also added a integration test for checking the backend api

---
+++ 04-using-actions/src/index.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "bun:test";
+
+test("2 * 2", async () => {
+  const result = await Promise.resolve(2 * 2);
+  expect(result).toEqual(4);
+});
+
+const baseUrl = "http://localhost:3000";
+test("GET /api/hello returns expected JSON response", async () => {
+  const response = await fetch(`${baseUrl}/api/hello`);
+  expect(response.status).toBe(200);
+  const json = await response.json();
+  expect(json).toEqual({
+    message: "Hello, world!",
+    method: "GET"
+  });
+});
